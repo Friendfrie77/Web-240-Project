c01a8b569d8162c2a9cf496b3ba3b40307317405
Friendfrie77 <awfriend77@gmail.com>
2022-07-10 00:28:47 -0400

    starting to set up index page

--- static/scripts/index-scripts.js
+++ static/scripts/index-scripts.js
@@ -1,9 +1,23 @@
+/* counter animations */
+document.addEventListener('DOMContentLoaded',function counters(){
+    var counter = document.getElementById('spaycounter');
+    counter.innerText = '0';
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+      const c = +counter.innerText
+      const increment = target/1000;
+      if(c < target){
+        counter.innerText= `${Math.ceil(c+increment)}`};
+        setTimeout(updateCounter, 1);
+    };
+    updateCounter();
+  });
 /* gsap index page animations */
 gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.create({
     trigger:"#section-1",
     start:"top top",
-    end: "bottom 150px",
+    end: "bottom 10px",
     pin: "#index-content-1"
 })
 
@@ -13,8 +27,5 @@ ScrollTrigger.create({
     end: "bottom 150ppx",
     pin:"#index-content-2"
 })
-/* flip card aninmations */
 
-/* setting constents */
-const pictures = gsap.utils.toArray(".cat-card");
   
